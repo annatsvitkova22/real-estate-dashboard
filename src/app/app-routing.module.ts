4ef54e7ce5f641d89ae7999c0421ab100c63a6c8
Anna Tsvitkova <anna.tsvitkova@outright.digital>
2020-06-10 16:00:47 +0300

    Create footer and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { PostComponent } from './post/post.component';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'flat', component: CardComponent },
+  { path: '', pathMatch: 'full', component: PostComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
